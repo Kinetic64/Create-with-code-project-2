2c7b7766668afb81ed67c279a4e7ed4fa143a7d1
Kinetic64 <ben.ward@simplygreat.education>
2026-06-18 13:58:10 +0100

    Added boundaries and move cookieeeeeeeeeee

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
     public InputAction moveAction;
     public Vector2 moveInput;
     public float speed = 10f;
+    public float xrange = 10f;
+    public float zrange = 10f;
+
+    public GameObject cookie;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,12 +20,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x < -10)
-        {
-transform.position = new Vector3 (transform.position.z)
-        }
+        //Stay inbounds
+       if(transform.position.x < -xrange)
+       {
+        transform.position = new Vector3(-xrange, transform.position.y, transform.position.z);
+       }
+
+       if(transform.position.x > xrange)
+       {
+        transform.position = new Vector3(xrange, transform.position.y, transform.position.z);
+       }
+
+       if(transform.position.z < -zrange)
+       {
+        transform.position = new Vector3(transform.position.x, transform.position.y, -zrange);
+       }
+
+       if(transform.position.z > zrange)
+       {
+        transform.position = new Vector3(transform.position.x, transform.position.y, zrange);
+       }
+
         moveInput = moveAction.ReadValue<Vector2>();
 
+        //Move about
         transform.Translate(Vector3.right * moveInput.x * Time.deltaTime * speed);
         transform.Translate(Vector3.forward * moveInput.y * Time.deltaTime * speed);
     }
